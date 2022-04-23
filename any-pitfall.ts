0fa9b5c01ef7086274403b7191475716e68f9e94
nhanluongoe <nhanlt18@gmail.com>
2022-04-23 17:53:24 +0700

    add any pitfall

---
+++ any-pitfall.ts
@@ -0,0 +1,6 @@
+const user: any = { name: "John" };
+const numbers: number[] = user; // no error, that's why we should avoid any as much as possible
+
+/**
+ * Using any is like we are using Javascript
+ */
