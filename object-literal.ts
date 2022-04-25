5bbe5ea11b35fa9d86612a6917867932ca1270a8
nhanluongoe <nhanlt18@gmail.com>
2022-04-25 22:25:13 +0700

    notes: add summary

--- object-literal.ts
+++ object-literal.ts
@@ -33,3 +33,9 @@ isCuddling({
   cuddling: true,
   extraProperty: "extra",
 }); // error
+
+/**
+ * In general, TypeScript allows objects to have extra properties. For example, we can pass a variable with the type {name: string, age: number} to a function expecting a {name: string}.
+ * But TypeScript doesn't allow extra properties when passing literal objects to functions, or assigning them directly to a variable with an explicit type. If we have function userName(user: {name: string}), then userName({name: 'Amir', age: 36}) is a type error. TypeScript knows that no code will ever access that age property, so there's no reason for it to exist.
+ * That can show us subtle bugs like misspelled property names.
+ */
