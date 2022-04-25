631eab79f77d98c4e8f9898709e01d4a0e17e83f
nhanluongoe <nhanlt18@gmail.com>
2022-04-25 20:47:53 +0700

    note: add example for index signature pitfall

--- index-signature.ts
+++ index-signature.ts
@@ -1,3 +1,4 @@
+// 1
 type userEmails = {
   [userName: string]: string;
 };
@@ -6,6 +7,12 @@ const emails: userEmails = { dan: "dan@example.com" };
 emails.belly; // no error ?!
 
 const bellyEmail: string = emails.belly; // no error ?!
+
+//2
+type CatAges = { [name: string]: number };
+const catAges: CatAges = { Keanu: 2 };
+const age: number = catAges.MsFluff;
+console.log(age); // no error, undefined
 
 /**
  * Can enable --noUncheckedIndexedAccess to change this behavior
