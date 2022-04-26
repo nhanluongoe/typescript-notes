adaa371fe4ea9627ef7e515ce84a4c613a146e62
nhanluongoe <nhanlt18@gmail.com>
2022-04-26 08:30:08 +0700

    note: add type predicates

---
+++ type-predicate.ts
@@ -0,0 +1,20 @@
+//1
+function isNumber(n: number | string): n is number {
+  return typeof n === "string";
+}
+const n: string = "not a number";
+const n2: number = isNumber(n) ? n : 0;
+
+console.log(n2); // 'not a number', no error
+
+//2
+function isString(input: unknown): input is string {
+  return typeof input === "string";
+}
+console.log(isString("hello")); // true
+console.log(isString({ input: "hello" })); // false
+
+/**
+ * TypeScript trusts us to implement type predicates correctly. Test your predicate functions well to make sure that they do what you think they do.
+ * Type predicates can take unions as their arguments, or they can take unknown. In most cases, unknown is the safest and most flexible option.
+ */
