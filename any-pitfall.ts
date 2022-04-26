0fae758fd8cf43a7f839b5714c4040928ad8fbe6
nhanluongoe <nhanlt18@gmail.com>
2022-04-27 05:27:26 +0700

    note: any and unknown in generic constraint

--- any-pitfall.ts
+++ any-pitfall.ts
@@ -1,6 +1,16 @@
+//1
 const user: any = { name: "John" };
 const numbers: number[] = user; // no error, that's why we should avoid any as much as possible
 
+//2
+type ItemObjects<T extends Array<unknown>> = {
+  items: T;
+};
+
+const numbersObject: ItemObjects<number[]> = { items: [1, 2, 3] }; //ok
+const stringsObject: ItemObjects<string[]> = { items: [1, 2, 3] }; //error
+
 /**
  * Using any is like we are using Javascript
+ * The only safe use of any or unknown is in generic constraint
  */
