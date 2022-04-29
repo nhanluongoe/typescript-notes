cd704c9b9c93b82dfdfdd1b8f0499cd245515b67
nhanluongoe <nhanlt18@gmail.com>
2022-04-29 15:07:20 +0700

    note: add explanation

--- keyof-generic-constraint.ts
+++ keyof-generic-constraint.ts
@@ -12,3 +12,11 @@ function get2<T, K extends keyof T>(obj: T, key: K) {
   return obj[key];
 }
 const name2: string = get2({ name: "John", age: 36 }, "name"); // no error, ok
+
+/**
+ * Explanation
+ * get's first type parameter is T, with no constraint. We passed a {name: string, age: number}, so that must be T.
+ * get's second type parameter is Key extends keyof T. We passed 'age', which is indeed a key of {name: string, age: number}, so that's allowed.
+ * The return type is declared as T[Key]. If we expand the type parameters out, that's {name: string, age: number}['age']. We can reduce that type to just number, so that's the return type.
+Putting those together, this generic function call's type is (obj: {name: string, age: number}, key: 'age') => number.
+ */
