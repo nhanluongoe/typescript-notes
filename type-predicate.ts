e46953595f9a6d76cfd93ae90e9c1c0cbbecd269
nhanluongoe <nhanlt18@gmail.com>
2022-04-29 08:04:36 +0700

    note: add filter with predicate

--- type-predicate.ts
+++ type-predicate.ts
@@ -14,6 +14,15 @@ function isString(input: unknown): input is string {
 console.log(isString("hello")); // true
 console.log(isString({ input: "hello" })); // false
 
+//3
+function isString1(input: string | number) {
+  return typeof input === "string";
+}
+
+const numbersAndStrings = [1, "2", 3, "4"];
+const onlyString: string[] = numbersAndStrings.filter(isString1); //error
+const onlyString2: string[] = numbersAndStrings.filter(isString); // okay, no error
+
 /**
  * TypeScript trusts us to implement type predicates correctly. Test your predicate functions well to make sure that they do what you think they do.
  * Type predicates can take unions as their arguments, or they can take unknown. In most cases, unknown is the safest and most flexible option.
