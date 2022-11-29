fa2739873d7c0dad59819b5e9fda58ca9bb0c751
nhanluongoe <nhanlt18@gmail.com>
2022-11-29 21:43:05 +0700

    chore: format code

--- decode-url-search-params.ts
+++ decode-url-search-params.ts
@@ -1,6 +1,6 @@
-import { String } from 'ts-toolbelt';
+import { String } from "ts-toolbelt";
 
-const query = `/home?a=foo&b=wow` 
+const query = `/home?a=foo&b=wow`;
 
 type Query = typeof query;
 type SecondQueryPart = String.Split<Query, "?">[1];
@@ -8,14 +8,17 @@ type QueryElements = String.Split<SecondQueryPart, "&">;
 
 type QueryParams = {
   [QueryElement in QueryElements[number]]: {
-    [K in String.Split<QueryElement, "=">[0]]: String.Split<QueryElement, "=">[1]
-  }
-}[QueryElements[number]]
+    [K in String.Split<QueryElement, "=">[0]]: String.Split<
+      QueryElement,
+      "="
+    >[1];
+  };
+}[QueryElements[number]];
 
-// we get auto complete here and ts will 
+// we get auto complete here and ts will
 // throw error if a param doesn't match
 const params: QueryParams = {
   a: "foo",
-  b: "wow"
-  c: 'not match'
-}
+  b: "wow",
+  c: "not match",
+};
