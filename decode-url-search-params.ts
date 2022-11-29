b65d80bf439df9801e9fcdace92cbfbff3992bd6
nhanluongoe <nhanlt18@gmail.com>
2022-11-29 21:42:15 +0700

    knowledge(tip): decode url search param at type level

---
+++ decode-url-search-params.ts
@@ -0,0 +1,21 @@
+import { String } from 'ts-toolbelt';
+
+const query = `/home?a=foo&b=wow` 
+
+type Query = typeof query;
+type SecondQueryPart = String.Split<Query, "?">[1];
+type QueryElements = String.Split<SecondQueryPart, "&">;
+
+type QueryParams = {
+  [QueryElement in QueryElements[number]]: {
+    [K in String.Split<QueryElement, "=">[0]]: String.Split<QueryElement, "=">[1]
+  }
+}[QueryElements[number]]
+
+// we get auto complete here and ts will 
+// throw error if a param doesn't match
+const params: QueryParams = {
+  a: "foo",
+  b: "wow"
+  c: 'not match'
+}
