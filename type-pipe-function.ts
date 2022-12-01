15504bd8ee715ec3ba9d167dcd6114056a0415f0
nhanluongoe <nhanlt18@gmail.com>
2022-12-01 21:30:44 +0700

    knowledge(tip): type pipe function

--- type-pipe-function.ts
+++ type-pipe-function.ts
@@ -1,15 +1,17 @@
-function compose<Input, FirstArg>(func: (input: Input) => FirstArg): FirstArg;
+function compose<Input, FirstArg>(
+  func: (input: Input) => FirstArg
+): (input: Input) => FirstArg;
 function compose<Input, FirstArg, SecondArg>(
   func1: (input: Input) => FirstArg,
   func2: (input: FirstArg) => SecondArg
-): SecondArg;
+): (input: Input) => SecondArg;
 function compose<Input, FirstArg, SecondArg, ThirdArg>(
   func1: (input: Input) => FirstArg,
   func2: (input: FirstArg) => SecondArg,
   func3: (input: SecondArg) => ThirdArg
-): ThirdArg;
-function compose(...args: any[]) {
-  return args.reduceRight((result, func) => func(result)) as any;
+): (input: Input) => ThirdArg;
+function compose(...functions: any[]) {
+  return (input: any) => functions.reduce((arg, fn) => fn(arg), input);
 }
 
 function addOne(num: number): number {
@@ -28,3 +30,7 @@ const addOneOnly = compose(addOne);
 const addOneToString = compose(addOne, numToString);
 // const errorComposition = compose(numToString, addOne);
 const addOneToStringToNum = compose(addOne, numToString, stringToNum);
+
+console.log(addOneOnly(1));
+console.log(addOneToString(1));
+console.log(addOneToStringToNum(1));
